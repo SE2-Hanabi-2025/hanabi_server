5ea291feada10872ff261bb3a738ff8a75c45596
gabriel.ranftler <gabriel.ranftler@gmx.at>
2025-03-28 19:00:28 +0100

    added springboot

---
+++ src/main/java/com/se2/hanabi/demo/HanabiServerApplication.java
@@ -0,0 +1,13 @@
+package com.se2.hanabi.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HanabiServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HanabiServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/se2/hanabi/demo/HanabiServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.se2.hanabi.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HanabiServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
