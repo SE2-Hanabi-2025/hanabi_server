1dfa56ea1ae2c73ca98ea8e14ff0174e1a72c1ec
krametter38 <154614400+krametter38@users.noreply.github.com>
2025-05-08 16:37:55 +0200

    fehler wurden behoben

--- src/main/java/se2/server/hanabi/services/LobbyCodeGenerator.java
+++ src/main/java/se2/server/hanabi/services/LobbyCodeGenerator.java
@@ -3,7 +3,7 @@ package se2.server.hanabi.services;
 import java.security.SecureRandom;
 
 public class LobbyCodeGenerator {
-    private static final String Characters = "^[A-NP-Z1-9]+$";
+    private static final String REGEX = "^[A-NP-Z1-9]+$";
     private static final int length = 6;
 
     private static final SecureRandom random = new SecureRandom();
@@ -11,8 +11,8 @@ public class LobbyCodeGenerator {
     public static String generateLobbyCode() {
         StringBuilder lobbyCode = new StringBuilder();
         for (int i = 0; i < length; i++) {
-            int index = random.nextInt(Characters.length());
-            lobbyCode.append(Characters.charAt(index));
+            int index = random.nextInt(REGEX.length());
+            lobbyCode.append(REGEX.charAt(index));
         }
         return lobbyCode.toString();
     }
