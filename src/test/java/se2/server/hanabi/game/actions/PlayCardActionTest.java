98697390c9c4310c8ebde1fa645aeb36edd7a187
thedovla <donjakvlado@gmail.com>
2025-05-11 15:10:29 +0200

    Added testCorrectCardAddedToStack()
    Added test to PlayCardActionTest class.
    Tests if playing the correct next card updates the stack correctly.
    Checks that a success message is returned.

--- src/test/java/se2/server/hanabi/game/actions/PlayCardActionTest.java
+++ src/test/java/se2/server/hanabi/game/actions/PlayCardActionTest.java
@@ -1,4 +1,30 @@
 package se2.server.hanabi.game.actions;
 
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import se2.server.hanabi.game.GameManager;
+import se2.server.hanabi.model.Card;
+import se2.server.hanabi.util.ActionResult;
+
+
+import java.util.List;
+import static org.junit.jupiter.api.Assertions.*;
+
 public class PlayCardActionTest {
+    private GameManager game;
+
+    @BeforeEach
+    public void setup() {
+        game = GameManager.createNewGame(List.of("Vlado", "Ermin"));
+    }
+
+    @Test
+    public void testCorrectCardAddedToStack() {
+        List<Card> hand = game.getHands().get("Vlado");
+        hand.clear();
+        hand.add(new Card(1, Card.Color.RED));
+        ActionResult result = new PlayCardAction(game, "Vlado", 0).execute();
+        assertTrue(result.getMessage().startsWith("You successfully played"));
+        assertEquals(1, game.getPlayedCards().get(Card.Color.RED));
+    }
 }
