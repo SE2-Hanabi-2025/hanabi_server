338140f286383fa99279a2e92831bba34442bedf
thedovla <donjakvlado@gmail.com>
2025-05-11 13:17:11 +0200

    Added testValidColorHintDecreasesTokens
    Added test in HintActionTest class, to test valid color hint reduces token count (hint token count decreases from 3 to 2).

--- src/test/java/se2/server/hanabi/game/actions/HintActionTest.java
+++ src/test/java/se2/server/hanabi/game/actions/HintActionTest.java
@@ -1,4 +1,32 @@
 package se2.server.hanabi.game.actions;
 
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import se2.server.hanabi.game.GameManager;
+import se2.server.hanabi.game.HintType;
+import se2.server.hanabi.model.Card;
+import se2.server.hanabi.util.ActionResult;
+
+import java.util.List;
+import static org.junit.jupiter.api.Assertions.*;
+
 public class HintActionTest {
+
+    private GameManager game;
+
+    @BeforeEach
+    public void setup() {
+        game = GameManager.createNewGame(List.of("Vlado", "Ermin"));
+    }
+
+    @Test
+    public void testValidColorHintDecreasesTokens() {
+        game.setHints(4);
+        List<Card> hand = game.getHands().get("Ermin");
+        hand.clear();
+        hand.add(new Card(2, Card.Color.BLUE));
+        ActionResult result = new HintAction(game, "Vlado", "Ermin", HintType.COLOR, Card.Color.BLUE).execute();
+        assertTrue(result.getMessage().contains("Hint given"));
+        asserEquals(3, game.getHints());
+    }
 }
