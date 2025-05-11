568c4389ac0ddabd964d8b6784cc3eda203bfed7
thedovla <donjakvlado@gmail.com>
2025-05-11 15:54:50 +0200

    Added testPerfectGameEnds()
    Added test to class PlayCardActionTest to ensure all stacks complete to trigger game over.
    Sets all color stacks to MAX_CARD_VALUE except one.
    After playing final card, game should end.

--- src/test/java/se2/server/hanabi/game/actions/PlayCardActionTest.java
+++ src/test/java/se2/server/hanabi/game/actions/PlayCardActionTest.java
@@ -49,4 +49,18 @@ public class PlayCardActionTest {
         ActionResult result = new PlayCardAction(game, "Ermin", 0).execute();
         assertEquals(GameRules.MAX_HINTS, game.getHints());
     }
+
+    @Test
+    public void testPerfectGameEnds() {
+        for (Card.Color color : Card.Color.values()) {
+            game.getPlayedCards().put(color, GameRules.MAX_CARD_VALUE);
+        }
+        game.getPlayedCards().put(Card.Color.RED, GameRules.MAX_CARD_VALUE -1);
+        List <Card> hand = game.getHands().get("Vlado");
+        hand.clear();
+        hand.add(new Card(5, Card.Color.RED));
+        ActionResult result = new PlayCardAction(game, "Vlado", 0).execute();
+        assertTrue(game.isGameOver());
+        assertTrue(result.getMessage().contains("Perfect!"));
+    }
 }
