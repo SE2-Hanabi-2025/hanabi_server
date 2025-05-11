f0e2484c564cbf2ef3c8e4420781e510ca45ca9a
thedovla <donjakvlado@gmail.com>
2025-05-11 18:01:15 +0200

    Add unique integer ID to Player class

--- src/main/java/se2/server/hanabi/model/Player.java
+++ src/main/java/se2/server/hanabi/model/Player.java
@@ -2,17 +2,23 @@ package se2.server.hanabi.model;
 
 public class Player {
     private String name;
+    private final int id;
+    private static int nextId = 0;
 
     public Player(String name) {
         this.name = name;
+        this.id = nextId++;
     }
 
     public String getName() {
         return name;
     }
 
-
     public void setName(String name) {
         this.name = name;
+    }
+
+    public int getId() {
+        return id;
     }
 }
